212297641570a648842e752b11881db42351d86e
Hans B <handihans@gmail.com>
2015-01-31 20:40:43 -0600

    cleaned up with bitwise operators during steganography, list comprehensions

--- wave_encode.py
+++ wave_encode.py
@@ -9,23 +9,18 @@ Hans
 import wave
 from sys import argv
 
-def write_string(string, data_list, start=0, space=1):
-    for i in range(0, len(string)):
-        data_index = start + i * space
-        if data_list[data_index] % 2 == int(string[i]) % 2:
-            pass
-        else:
-            # Prevents values in data from going out of range(0, 256)
-            # and raising ValueError with bytes() function
-            if data_list[data_index] >= 255:
-                data_list[data_index] -= 1
+# 8-bit binary string 'b', wave bytearray data 'w'
+def encode(b, w, start=0, step=1):
+    for i, bit in enumerate(b):
+            if bit == '0':
+                w[start+i*step] = w[start+i*step] & 254
             else:
-                data_list[data_index] += 1
-    return data_list
+                w[start+i*step] = w[start+i*step] | 1
+    return w
 
 if argv[1] == '--help':
     print('Usage: python3 [SCRIPT] [TEXTFILE] [WAVEFILE]')
-    print('[SCRIPT] is the name of this script')
+    print('[SCRIPT] is the path to this script')
     print('[TEXTFILE] must be in ASCII format.')
     print('[WAVEFILE] must be a .wav file.')
     print()
@@ -35,23 +30,14 @@ if argv[1] == '--help':
 
 else:
     script, textfile, wavefile = argv
-
+    
     print('Creating binary data of textfile...')
-    textfile = open(textfile, 'r')
-    instring  = ''
-    for i in textfile:
-        instring += i
-    textfile.close()
+    f = open(textfile, 'r')
+    string = f.read()
+    f.close()
 
-    # Convert each item in 'instring' to 8 bit binary number
-    instring = list(instring)
-    for i in range(len(instring)):
-        instring[i] = format(ord(instring[i]), '08b')
-
-    # Create continuous binary string
-    bin_string = ''
-    for i in instring:
-        bin_string += i
+    # Convert intstring into 8-bit binary string
+    bin_string = ''.join(format(i, '08b') for i in bytearray(string, 'ascii'))
 
     # Create name of copy file in the form 'filename(x).wav'
     for i in range(1, 100):
@@ -64,34 +50,30 @@ else:
 
     print('Reading wavefile...')
     # Create empty .wav file with same properties as original file
-    w = wave.open(wavefile, 'rb')
-    data = w.readframes(w.getnframes())
+    w = wave.open(wavefile, 'r')
+    data = bytearray(w.readframes(w.getnframes()))
     params = w.getparams()
-
-    output = wave.open(outfile, 'wb')
-    output.setparams(params)
-
     w.close()
-    data = list(data)
+    output = wave.open(outfile, 'w')
+    output.setparams(params)
 
     print('Editing wave data...')
     # Add 8 bit integer to beginning of data specifying
     # the spacing between each bit written to data
     spacing = int( (len(data)-8) / len(bin_string) )
     b_spacing = format(spacing, '08b')
-
+    
     # Subtract 8 because first byte is used to store spacing
     if len(bin_string) > len(data) - 8:
-        print('SizeError: Input string too long.')
+        print('ERROR: Input file too big.')
     else:
         # Write data to least significant bits of wave data
-        write_string(b_spacing, data)
-        write_string(bin_string, data, 8, spacing)
+        data = encode(b_spacing, data)
+        data = encode(bin_string, data, 8, spacing)
 
-    data = bytes(data)
-
-    print('Writing data to new wavefile...')
-    # Finish up
-    output.writeframes(data)
+        # Finish up
+        print('Writing data to new wavefile...')
+        output.writeframes(bytearray(data))
+        
     output.close()
 
